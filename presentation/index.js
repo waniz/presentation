b46bdcf75b535a8ee5000408832c8c73726317ab
Dmitry <waniz@mail.ru>
2016-11-08 23:42:30 +0300

    First section

--- presentation/index.js
+++ presentation/index.js
@@ -40,7 +40,9 @@ const images = {
   city: require("../assets/city.jpg"),
   kat: require("../assets/kat.png"),
   logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  markdown: require("../assets/markdown.png"),
+  kelly: require("../assets/kelly.jpg"),
+  cover: require("../assets/cover.jpg")
 };
 
 preloader(images);
@@ -54,21 +56,27 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+          <Slide transition={["zoom"]} bgColor="black">
+            <Image src={images.kelly.replace("/", "")} margin="0px auto 40px" height="500px"/>
+            <Image src={images.cover.replace("/", "")} margin="0px auto 40px" height="500px"/>
+            <Heading size={1} fit caps lineHeight={1} textColor="orange">
+              Келли Макгонигал, Сила воли. Как развить и укрепить.
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+              <Text textSize="0.4em" margin="0px auto 40px" bold textColor="yellow">Лучше сразу скажи нет :)</Text>
           </Slide>
+      <Slide transition={["zoom"]} bgColor="black">
+      </Slide>
+
+
+
+
+
+
+
+
+
+
+
           <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
             <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
             <Heading size={2} caps fit textColor="primary" textFont="primary">
