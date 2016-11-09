63d074a2677e30ff77738dc37003024b77e4c154
Dmitry <waniz@mail.ru>
2016-11-09 07:40:15 +0300

    some slides

--- presentation/index.js
+++ presentation/index.js
@@ -38,17 +38,20 @@ require("spectacle/lib/themes/default/index.css");
 
 const images = {
   city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
   logo: require("../assets/formidable-logo.svg"),
   markdown: require("../assets/markdown.png"),
   kelly: require("../assets/kelly.jpg"),
-  cover: require("../assets/cover.jpg")
+  cover: require("../assets/cover.jpg"),
+  layout: require("../assets/layout.jpg"),
+  brain_1: require("../assets/brain_willpower.png"),
+  brain_2: require("../assets/brain_willpower2.png"),
+  cheesecake: require("../assets/cheesecake.jpg")
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "black"
 });
 
 export default class Presentation extends React.Component {
@@ -56,6 +59,7 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
+
           <Slide transition={["zoom"]} bgColor="black">
             <Image src={images.kelly.replace("/", "")} margin="0px auto 40px" height="500px"/>
             <Image src={images.cover.replace("/", "")} margin="0px auto 40px" height="500px"/>
@@ -64,25 +68,58 @@ export default class Presentation extends React.Component {
             </Heading>
               <Text textSize="0.4em" margin="0px auto 40px" bold textColor="yellow">Лучше сразу скажи нет :)</Text>
           </Slide>
-      <Slide transition={["zoom"]} bgColor="black">
-      </Slide>
 
 
+          <Slide transition={["zoom"]} bgImage={images.layout.replace("/", "")}>
+            <Appear fid="1">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="black">1. Зачем нам сила воли?</Text>
+            </Appear>
+            <Appear fid="2">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="black">2. Как все устроено?</Text>
+            </Appear>
+            <Appear fid="3">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="black">3. Самоконтроль и импульсивная составляющая. Неосознанные выборы ':tramp'</Text>
+            </Appear>
+            <Appear fid="4">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="black">4. Тренировка воли</Text>
+            </Appear>
+            <Image src={images.brain_1.replace("/", "")} margin="0px auto 40px" height="200px"/>
+            <Image src={images.brain_2.replace("/", "")} margin="0px auto 40px" height="200px"/>
 
-
-
-
-
-
-
-
-
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
           </Slide>
+
+
+          <Slide transition={["zoom"]} bgImage={images.cheesecake.replace("/", "")}>
+            <Appear fid="1">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="orange">1. Почему иногда легко сопротивляться чизкейку, а иногда нет</Text>
+            </Appear>
+            <Appear fid="2">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="black">2. Защитные рефлексы мозга</Text>
+            </Appear>
+            <Appear fid="3">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="black">3. pause-and-plan, fight-or-flight</Text>
+            </Appear>
+            <Appear fid="4">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="black">4. Реакция сердца и дыхания и зрачков на вкусняшку и угрозу</Text>
+            </Appear>
+            <Appear fid="4">
+              <Text textSize="1em" margin="0px auto 40px" bold textColor="white">5. Цена постоянного самоконтроля</Text>
+            </Appear>
+          </Slide>
+
+          <Slide transition={["zoom"]} bgImage={images.cheesecake.replace("/", "")}>
+
+          </Slide>
+
+
+
+
+
+
+
+
+
+
           <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
             <CodePane
               lang="jsx"
